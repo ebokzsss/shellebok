e012b701a977782c600bfd62d3254478d02262f7
ebokzsss <84223726+ebokzsss@users.noreply.github.com>
2024-02-22 17:08:21 +0700

    Update and rename index.phpv to index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+        <ul style="display:none">
+            <li style="color: #000000;"><a href="https://rekrutmen.bkpsdm.banggaikep.go.id/js/-/SLOTGARANSI/">Slot Garansi</a></li> 
+            <li style="color: #000000;"><a href="http://sisdesk.banjarmasinkota.go.id/assets/-/mbok-pulsa/">http://sisdesk.banjarmasinkota.go.id/assets/-/mbok-pulsa/</a></li> 
+            <li style="color: #000000;"><a href="https://arsipsurat.kominfo.tanjungpinangkota.go.id/admin/assets/img/maxwin-aja/">https://arsipsurat.kominfo.tanjungpinangkota.go.id/admin/assets/img/maxwin-aja/</a></li> 
+            <li style="color: #000000;"><a href="https://esisinfo-bpsdm.kemenkumham.go.id/lampiran/-/slot-terbaik/">https://esisinfo-bpsdm.kemenkumham.go.id/lampiran/-/slot-terbaik/</a></li> 
+            <li style="color: #000000;"><a href="http://smartgov.acehutara.go.id/cti/-/mbokslot/">http://smartgov.acehutara.go.id/cti/-/mbokslot/</a></li> 
+            <li style="color: #000000;"><a href="https://sptjm.lldikti4.id/fonts/-/slot-menang/>https://sptjm.lldikti4.id/fonts/-/slot-menang/</a></li> 
+            <li style="color: #000000;"><a href="http://dukupuntang.desa.cirebonkab.go.id/moonstar/maxwin-cu/">http://dukupuntang.desa.cirebonkab.go.id/moonstar/maxwin-cu/</a></li> 
+            <li style="color: #000000;"><a href="https://eoffice.pal.co.id/images/-/pulsa-maxwin/">https://eoffice.pal.co.id/images/-/pulsa-maxwin/</a></li> 
+            <li style="color: #000000;"><a href="https://santika.upnjatim.ac.id/products/pastiwin-gacor/>pastiwin777</a></li> 
+            <li style="color: #000000;"><a href="https://jdih.dprd.subang.go.id/website/-/pw777/">https://jdih.dprd.subang.go.id/website/-/pw777/</a></li> 
+            <li style="color: #000000;"><a href="https://sikapjawara.kemenkumham.go.id/js/-/pulsa-maxwin/">https://sikapjawara.kemenkumham.go.id/js/-/pulsa-maxwin/</a></li> 
+            <li style="color: #000000;"><a href="https://simawa.uai.ac.id/application/-/Slot-Thailand/">https://simawa.uai.ac.id/application/-/Slot-Thailand/</a></li> 
+            <li style="color: #000000;"><a href="https://fmipa.unand.ac.id/awstatsicons/-/pulsa-maxwin/">https://fmipa.unand.ac.id/awstatsicons/-/pulsa-maxwin/</a></li> 
+            <li style="color: #000000;"><a href="https://old-eoffice.bmkg.go.id/v7/-/ngantuk-rank-1/">https://old-eoffice.bmkg.go.id/v7/-/ngantuk-rank-1/</a></li> 
+            <li style="color: #000000;"><a href="http://jurnal.pusjatan.pu.go.id/files/-/slot-terbaik/">Slot Terbaik</a></li> 
+            <li style="color: #000000;"><a href="https://old-eoffice.bmkg.go.id/V7/-/maxw1n-gas/">https://old-eoffice.bmkg.go.id/V7/-/maxw1n-gas/</a></li> 
+            <li style="color: #000000;"><a href="http://pmb.unipar.ac.id/vendor/-/polisi-slot/">http://pmb.unipar.ac.id/vendor/-/polisi-slot/</a></li> 
+            <li style="color: #000000;"><a href="http://103.87.196.148/e-proposal/img/-/slot-maxwin/">http://103.87.196.148/e-proposal/img/-/slot-maxwin/</a></li> 
+            <li style="color: #000000;"><a href="http://103.111.58.142/storage/-/mbok-slot/">mbok slot</a></li> 
+            <li style="color: #000000;"><a href="https://jawara.kemenkumham.go.id/js/-/gacor-maxwin/">https://jawara.kemenkumham.go.id/js/-/gacor-maxwin/</a></li> 
+            <li style="color: #000000;"><a href="http://103.211.50.163/digital-backup/-/gacormaxwin/">http://103.211.50.163/digital-backup/-/gacormaxwin/</a></li> 
+            <li style="color: #000000;"><a href="https://e-office.unand.ac.id/new_oldini%20versi%20yg%20lama%20tidak%20di%20load/assets/-/akun-pro-thailand/">akun pro thailand</a></li> 
+            <li style="color: #000000;"><a href="https://pengumuman.unsil.ac.id/images/-/gampang-maxwin/">https://pengumuman.unsil.ac.id/images/-/gampang-maxwin/</a></li> 
+            <li style="color: #000000;"><a href="https://esisinfo-bpsdm.kemenkumham.go.id/lampiran/-/maxwin-gacor/">https://esisinfo-bpsdm.kemenkumham.go.id/lampiran/-/maxwin-gacor/</a></li> 
+            <li style="color: #000000;"><a href="https://sikd.unimed.ac.id/FilesUploaded/-/gopay-gacor/">http://kpa.cirebonkab.go.id/vendor/</a>http://kpa.cirebonkab.go.id/vendor/">http://kpa.cirebonkab.go.id/vendor/</li> 
+            <li style="color: #000000;"><a href="https://kayuh-lakasi.banjarmasinkota.go.id/img/-/pastiwin-hoki/">https://kayuh-lakasi.banjarmasinkota.go.id/img/-/pastiwin-hoki/</a></li> 
+            <li style="color: #000000;"><a href="https://sikd.unimed.ac.id/FilesUploaded/-/thailand-gacor/">https://sikd.unimed.ac.id/FilesUploaded/-/thailand-gacor/</a></li> 
+ </ul>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
+?>
