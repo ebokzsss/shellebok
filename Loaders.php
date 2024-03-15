48fdfc12bdfc96d90ff0120e79b893b6bd2deea3
ebokzsss <84223726+ebokzsss@users.noreply.github.com>
2024-03-15 08:15:25 +0700

    Update Loader.php

--- Loaders.php
+++ Loaders.php
@@ -60,7 +60,7 @@ function show_login_page($message = "")
 }
 
 if (!isset($_SESSION['authenticated'])) {
-    $stored_hashed_password = '$2y$10$wo6jo4pXk52iOcWRJGdG.OqDEUXEuGkOUfsMOQfOEa/FBcCmQq8Ou'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
+    $stored_hashed_password = '$2y$10$Evu4xfDvLMQPQa0kOtFLkOdUMNL4U4XrjIK/koLqHUs8CjQ1DZrdu'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
 
     if (isset($_POST['pass']) && password_verify($_POST['pass'], $stored_hashed_password)) {
         $_SESSION['authenticated'] = true;
